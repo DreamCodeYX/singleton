95c062a91078b5c851697d17ceb4be3009f28c00
DreamCodeYX <18260007202@163.com>
2019-03-12 14:17:31 +0800

    枚举和注入的方式写单例

---
+++ src/register/EnumSingleton.java
@@ -0,0 +1,20 @@
+package register;
+
+/**
+ * 枚举
+ */
+//常量中去使用，常量不就是用来大家都能够共用吗？
+//通常在通用API中使用
+public  enum EnumSingleton {
+    INSTANCE;
+    private Object data;
+    public Object getData() {
+        return data;
+    }
+    public void setData(Object data) {
+        this.data = data;
+    }
+    public static EnumSingleton getInstance(){
+        return INSTANCE;
+    }
+}
